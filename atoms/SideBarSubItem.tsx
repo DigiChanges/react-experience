ecbdc4eb53f0b1073abe2f3961d197e0aadaa633
JuaniGidoni <juan.gidoni@gmail.com>
2021-02-11 11:30:43 -0300

    refactor: sidebaritem sidebarsubitem

--- atoms/SideBarSubItem.tsx
+++ atoms/SideBarSubItem.tsx
@@ -11,12 +11,20 @@ const SideBarSubItem = ({theKey, name, path, equalPath, icon}) =>
 	return (
 		<Link href={path} key={theKey}>
 			<a className={`w-full flex flex-row items-center justify-center md:justify-start h-12 transform hover:translate-y-2 hover:translate-x-0 md:hover:translate-x-2 md:hover:translate-y-0 transition-transform ease-in duration-200 ${equalPath.subEqual ? 'text-yellow-100' : 'text-gray-500 hover:text-gray-800'} cursor-pointer`}>
-				{(Icon ? <span className="inline-flex flex-grow items-center justify-center md:justify-start h-6 w-12 text-lg text-gray-300">
-            <Icon size={size} color={color}/>
-          </span> : <span className="inline-flex flex-grow items-center justify-center h-6 w-12 text-lg text-gray-300"/>)}
+				{(
+
+				Icon ? 
+				<span className="inline-flex flex-grow items-center justify-center md:justify-start h-6 w-12 text-lg text-gray-300">
+           			 <Icon size={size} color={color}/>
+          		</span> 
+				: 
+				<span className="inline-flex flex-grow items-center justify-center h-6 w-12 text-lg text-gray-300"/>
+				
+				)}
+
 				<span className="text-sm font-medium hidden md:block justify-start md:justify-start">
-            {name}
-          </span>
+					{name}
+				</span>
 			</a>
 		</Link>
 	)
--- molecules/SidebarItem.tsx
+++ molecules/SidebarItem.tsx
@@ -28,46 +28,73 @@ const SideBarItem = ({theKey, name, path, equalPath, icon, levels, user, userPer
       isLoading && open ? setOpen(!open) : '';
 			const SubIcon = prop?.icon ? prop.icon : false;
 			return hasPermission(prop.permission, user) &&
-                <SideBarSubItem key={k} theKey={k} name={prop.name} path={prop.path} equalPath={equalPath.currentPath == prop.path ? {subEqual: true, subCurrentPath: equalPath.currentPath} : false} icon={SubIcon}/>
+                <SideBarSubItem 
+				key={k}
+				theKey={k}
+				name={prop.name}
+				path={prop.path}
+				equalPath={
+					equalPath.currentPath == prop.path ?
+					 {subEqual: true, subCurrentPath: equalPath.currentPath}
+					  : false
+					}
+				icon={SubIcon}/>
 		}) : '');
 
 	return (
-		<li className="justify-center" key={theKey}>
-			{multi ? (
-				<div>
-					<a onClick={toggleMenu}
-					   className={`w-full flex flex-row items-center justify-center md:justify-start h-12 transform hover:translate-y-2 hover:translate-x-0 md:hover:translate-x-2 md:hover:translate-y-0 transition-transform ease-in duration-200 ${equalPath.equal ? 'text-yellow-100' : 'text-gray-500 hover:text-gray-800'} cursor-pointer`}>
-						{(Icon ?
-							<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-              <Icon size={size} color={color}/>
-            </span> : <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400" />)}
-						<span className="text-sm font-medium hidden md:block">
-              {name}
-            </span>
-						{open && multi ? <span className="inline-flex items-center justify-end">
-              -
-              </span> : !open && multi ? <span className="inline-flex items-center justify-end">
-              +
-              </span> : ''}
-					</a>
-					<div className={open ? 'dropdown-menu block w-100 flex flex-col pl-5' : 'dropdown-menu hidden w-full'}>
-						{open && multi ? getDropDownItems() : ''}
-					</div>
+	<li className="justify-center" key={theKey}>
+		{multi ? (
+			<div>
+				<a onClick={toggleMenu}
+					className={`w-full flex flex-row items-center justify-center md:justify-start h-12 transform hover:translate-y-2 hover:translate-x-0 md:hover:translate-x-2 md:hover:translate-y-0 transition-transform ease-in duration-200 ${equalPath.equal ? 'text-yellow-100' : 'text-gray-500 hover:text-gray-800'} cursor-pointer`}>
+					{(
+						Icon ?
+						<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+							<Icon size={size} color={color}/>
+						</span> 
+						: 
+						<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"></span>
+					)}
+
+					<span className="text-sm font-medium hidden md:block">
+						{name}
+					</span>
+
+					{open && multi ? 
+					<span className="inline-flex items-center justify-end">
+						-
+						</span>
+						: !open && multi ? 
+						<span className="inline-flex items-center justify-end">
+						+
+					</span> 
+						: ''
+					}
+				</a>
+
+				<div className={open ? 'dropdown-menu block w-100 flex flex-col pl-5' : 'dropdown-menu hidden w-full'}>
+					{open && multi ? getDropDownItems() : ''}
 				</div>
-			) : (
-				<Link href={path} key={theKey}>
-					<a className={`flex flex-row items-center justify-center md:justify-start h-12 transform hover:translate-y-2 hover:translate-x-0 md:hover:translate-x-2 md:hover:translate-y-0 transition-transform ease-in duration-200  ${equalPath.equal ? 'text-yellow-100' : 'text-gray-500 hover:text-gray-800'} cursor-pointer`}>
-						{(Icon ?
-							<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-            <Icon size={size} color={color}/>
-          </span> : <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"/>)}
-						<span className="text-sm font-medium hidden md:block">
-            {name}
-          </span>
-					</a>
-				</Link>
-			)}
-		</li>
+
+			</div>
+		) : (
+			<Link href={path} key={theKey}>
+				<a className={`flex flex-row items-center justify-center md:justify-start h-12 transform hover:translate-y-2 hover:translate-x-0 md:hover:translate-x-2 md:hover:translate-y-0 transition-transform ease-in duration-200  ${equalPath.equal ? 'text-yellow-100' : 'text-gray-500 hover:text-gray-800'} cursor-pointer`}>
+					{(
+						Icon ?
+						<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+							<Icon size={size} color={color}/>
+						</span>
+						: 
+						<span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"/>
+					)}
+					<span className="text-sm font-medium hidden md:block">
+						{name}
+					</span>
+				</a>
+			</Link>
+		)}
+	</li>
 	)
 }
 
