faa5580a9f8cb578a6b6777f7e69bfbc773d10e0
Pablo R  Romero <46016525+90PabloRomero@users.noreply.github.com>
2021-03-09 12:00:24 -0300

    add show password function (#17)
    * agrego show password
    
    * fix/console-warn/PasswordShowHide.tsx

---
+++ templates/login/PasswordShowHide.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const PasswordShowHide = ({ field }) => {
+  const [showHidePassword, changeShowHidePassword] = useState(false);
+
+  return (
+    <span className="flex">
+      <input
+        type={showHidePassword ? "text" : "password"}
+        {...field}
+        className="w-5/6 bg-gray-800 border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2 hover:border-grey rounded shadow"
+        placeholder="Password"
+      />
+      <i
+        role="button"
+        tabIndex={0}
+        className="w-1/6 inline-block border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2 hover:border-grey rounded shadow"
+        onClick={() => changeShowHidePassword(!showHidePassword)}
+        aria-hidden="true"
+      >
+        show
+      </i>
+    </span>
+  );
+};
+
+export default PasswordShowHide;
