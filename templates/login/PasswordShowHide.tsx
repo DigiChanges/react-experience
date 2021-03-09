e35bec7ae3846accf8698cdc3c7dc92a499b531f
Pablo R Romero <90pabloromero@gmail.com>
2021-03-08 22:48:19 -0300

    agrego show password

--- templates/login/PasswordShowHide.tsx
+++ templates/login/PasswordShowHide.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const PasswordShowHide = ({ field }) => {
+const PasswordShowHide = ({ field, form }) => {
   const [showHidePassword, changeShowHidePassword] = useState(false);
 
   return (
@@ -8,15 +8,14 @@ const PasswordShowHide = ({ field }) => {
       <input
         type={showHidePassword ? "text" : "password"}
         {...field}
-        className="w-5/6 focus:outline-none text-base px-2 py-2 shadow"
+        className="w-5/6 bg-gray-800 border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2 hover:border-grey rounded shadow"
         placeholder="Password"
       />
       <i
         role="button"
         tabIndex={0}
-        className="w-1/6 inline-block focus:outline-none px-2 py-2 hover:border-grey shadow"
+        className="w-1/6 inline-block border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2 hover:border-grey rounded shadow"
         onClick={() => changeShowHidePassword(!showHidePassword)}
-        aria-hidden="true"
       >
         show
       </i>
