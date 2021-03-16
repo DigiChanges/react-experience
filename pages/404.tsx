ad804a3bd62506834ff9dc0a5c2c8ad0225ec4c3
Matias Ovejero <61472081+MatiasOvejero@users.noreply.github.com>
2021-03-16 16:20:38 -0300

    feat : create error 404, 500 & status error, plus Gilroy font add

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="gilroy">
+      <h1 className="text-7x1 text-justify mx-1/4 ">404</h1>
+      <h2 className="text-5xl text-left mx-1/4">Page Not Found!</h2>
+    </div>
+  );
+}
