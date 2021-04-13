048b19df0b08a73d5c421eba1dca51c6aadda637
Pablo R Romero <90pabloromero@gmail.com>
2021-04-12 21:22:42 -0300

    new rebase wip sidebar

--- templates/users/UserList.tsx
+++ templates/users/UserList.tsx
@@ -60,17 +60,11 @@ const UserList = ({ usersList, query, viewMore }) => {
           labelButtonName="Create User"
           icon={IconPlus}
           buttonAction={actionCreateButton}
-          className="text-3xl font-bold"
         />
       </div>
-      {/*     <div className="mx-1/12 pl-4 md:pl-0">
-
-        <div className="mt-6 flex flex-col justify-between w-5/6">
-          <FilterSort actionFilter={onClickFilter} />
-        </div> */}
       <div className="text-gray-500 bg-gray-900 flex flex-row items-center flex-wrap w-full justify-start">
         {usersList && usersList.map((user, i) => (
-          <MediaObject key={i} className="md:max-w-xs w-full md:w-1/2 p-2 bg-main-gray-600 h-18 rounded-lg mx-1 my-1 flex items-center font-semibold text-sm text-main-gray-250">
+          <MediaObject key={i} className="md:max-w-md w-full md:w-1/2 p-2 bg-main-gray-600 h-18 rounded-lg mx-1 my-1 flex items-center font-semibold text-sm text-main-gray-250">
             <div className="flex-col w-10 h-10 bg-white text-black justify-center content-center rounded-full">a</div>
             <div className="flex-col justify-center content-center ml-3">
               <Title titleType="h6" className="h-6 font-bold">{user.firstName}{' '}{user.lastName}</Title>
@@ -91,7 +85,6 @@ const UserList = ({ usersList, query, viewMore }) => {
       {booleanConfirmDelete ? (
         <ConfirmDeleteUser close={openConfirmDelete} />
       ) : null}
-      {/* </div> */}
     </section>
   );
 };
